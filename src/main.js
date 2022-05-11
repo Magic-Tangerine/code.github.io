3b5e072ae2c567be9ef1f3deab23f977ec933e8e
lovestories0eng <1494121350@qq.com>
2022-05-11 18:51:55 +0800

    delete VueResource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
+// import VueResource from 'vue-resource'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -16,7 +16,7 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-Vue.use(VueResource)
+// Vue.use(VueResource)
 
 Vue.use(VueRouter)
 
